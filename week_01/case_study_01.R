cedf11c9480e5baddb0b8fe3b7ab5fce083fcb5a
JohnMernitz <jack.mernitz@gmail.com>
2024-12-12 22:18:11 -0500

    Update Case Study 1
    Local version control, oops

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,17 @@
-data(iris)
+##Case study 1 9/3/24
+#loading
+data("iris")
 
+install.packages("ggplot2")
+library(ggplot2)
+#summary
+summary(iris)
+
+#store mean as named object
+petal_length_mean <- mean(iris$Petal.Length)
+
+#histogram petal length
+hist(iris$Petal.Length)
+
+ggplot() +
+geom_histogram(aes(iris$Petal.Length))
